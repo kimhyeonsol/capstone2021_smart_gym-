716cfb99c65741e9924528ebf17317d1f1fd5a8b
김현솔 <tina980814@naver.com>
2021-10-06 16:12:32 +0900

    spring_init

---
+++ smartGym_backend/src/main/java/capstone2021/smartGym_backend/SmartGymBackendApplication.java
@@ -0,0 +1,13 @@
+package capstone2021.smartGym_backend;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SmartGymBackendApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(SmartGymBackendApplication.class, args);
+	}
+
+}
---
+++ smartGym_backend/src/test/java/capstone2021/smartGym_backend/SmartGymBackendApplicationTests.java
@@ -0,0 +1,13 @@
+package capstone2021.smartGym_backend;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SmartGymBackendApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}

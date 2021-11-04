24a0475280f16aba3ffe156a6afc7e2f6b8ee100
Hyerin <9993hye@naver.com>
2021-11-04 17:30:58 +0900

    1

--- smartGym_backend/src/main/java/capstone2021/smartGym_backend/controller/EquipmentController.java
+++ smartGym_backend/src/main/java/capstone2021/smartGym_backend/controller/EquipmentController.java
@@ -11,7 +11,7 @@ import org.springframework.web.bind.annotation.*;
 import java.io.IOException;
 import java.util.List;
 
-@Controller
+@Controller1
 public class EquipmentController {
     private final EquipmentService equipmentService;
 

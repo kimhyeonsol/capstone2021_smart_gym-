ef4a6bdeec5da8db60ffb568131e69f2f3317729
김현솔 <tina980814@naver.com>
2021-11-23 21:14:21 +0900

    실시간 csv 파일 자동 생성 기능 구현, recentReservation 메소드 수정, ftp 연결 및 테스트 완료

--- smartGym_backend/src/main/java/capstone2021/smartGym_backend/service/ESLService.java
+++ smartGym_backend/src/main/java/capstone2021/smartGym_backend/service/ESLService.java
@@ -14,7 +14,7 @@ public interface ESLService {
     boolean eslDelete(ESLDeleteDetailedReadDTO eslDeleteDetailedReadDTO); //ESL 삭제
     List<ESL> eslRead(); //ESL 조회
     ReturnESLDetailedRead eslDetailedRead(ESLDeleteDetailedReadDTO eslDeleteDetailedReadDTO); //ESL 상세조회
-    void eslReservationUpdate();
+    void eslReservationUpdate() throws Exception;
     String makeCsvStringAndReservationMatching(Equipment equipment, ESL esl,ESL newEsl);
     void writeCSV(String csvString);
     String recentReservation(Equipment equipment); //현재 시간 기준 가장 최근 예약 조회(모두 사용 가능 상태일 경우)

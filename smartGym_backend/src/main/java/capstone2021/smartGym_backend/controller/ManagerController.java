1354690bd31bd0c6b1e48ba5b9b14646e0041950
Hyerin <9993hye@naver.com>
2021-11-26 14:51:25 +0900

    Manager Login 프론트 요구사항 반영

--- smartGym_backend/src/main/java/capstone2021/smartGym_backend/controller/ManagerController.java
+++ smartGym_backend/src/main/java/capstone2021/smartGym_backend/controller/ManagerController.java
@@ -1,13 +1,11 @@
 package capstone2021.smartGym_backend.controller;
 
+import capstone2021.smartGym_backend.DTO.Manager.ManagerCheckLoginDTO;
 import capstone2021.smartGym_backend.DTO.Manager.ManagerLoginDTO;
 import capstone2021.smartGym_backend.service.ManagerService;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Controller;
-import org.springframework.web.bind.annotation.CrossOrigin;
-import org.springframework.web.bind.annotation.PostMapping;
-import org.springframework.web.bind.annotation.RequestBody;
-import org.springframework.web.bind.annotation.ResponseBody;
+import org.springframework.web.bind.annotation.*;
 
 @Controller
 public class ManagerController {
@@ -23,5 +21,19 @@ public class ManagerController {
     @ResponseBody
     public int managerLogin(@RequestBody final ManagerLoginDTO managerLoginDTO){
         return managerService.managerLogin(managerLoginDTO);
+    }
+
+    @CrossOrigin("*")
+    @PostMapping("/manager/loginAndlogout") //로그인&로그아웃
+    @ResponseBody
+    public boolean managerLoginAndLogout(@RequestBody final ManagerCheckLoginDTO managerCheckLoginDTO){
+        return managerService.managerCheckLogin(managerCheckLoginDTO);
+    }
+
+    @CrossOrigin("*")
+    @GetMapping("/manager/isLogin") //로그인 여부
+    @ResponseBody
+    public boolean managerIsLogin(){
+        return managerService.managerIsLogin();
     }
 }
--- smartGym_backend/src/main/java/capstone2021/smartGym_backend/domain/Manager.java
+++ smartGym_backend/src/main/java/capstone2021/smartGym_backend/domain/Manager.java
@@ -1,5 +1,7 @@
 package capstone2021.smartGym_backend.domain;
 
+import com.sun.istack.NotNull;
+
 import javax.persistence.Column;
 import javax.persistence.Entity;
 import javax.persistence.Id;
@@ -10,11 +12,23 @@ public class Manager {
     @Column(name="manager_password", length = 20, columnDefinition = "VARCHAR(20) default '0000'")
     private String managerPassword = "0000";
 
+    @Column(name="manager_check_login", length = 20)
+    @NotNull //널 허용 X
+    private String managerCheckLogin;
+
     public String getManagerPassword() {
         return managerPassword;
     }
 
     public void setManagerPassword(String managerPassword) {
         this.managerPassword = managerPassword;
+    }
+
+    public String getManagerCheckLogin() {
+        return managerCheckLogin;
+    }
+
+    public void setManagerCheckLogin(String managerCheckLogin) {
+        this.managerCheckLogin = managerCheckLogin;
     }
 }
